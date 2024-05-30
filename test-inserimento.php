61428f298b467d4344cf550d5406c061a9892f3d
patrick <pizza.rick05@gmail.com>
2024-05-30 14:04:41 +0200

    aggiunti file per testing

---
+++ test-inserimento.php
@@ -0,0 +1,30 @@
+<?php
+    include("connect.php");
+
+    echo"<br><br>";
+
+    $nome = $_POST['nome'];
+    $titolo = $_POST['titolo'];
+    $paragrafo = $_POST['paragrafo'];
+    $data = date("Y/m/d");
+    $ora = date("H:i:s");
+    
+    echo "Nome: $nome <br>";
+    echo "Titolo: $titolo <br>";
+    echo "Paragrafo: $paragrafo <br>";
+    echo "Data: $data <br>";
+    echo "Ora: $ora";
+
+    $query = "INSERT INTO `ricordi`(`nome`, `titolo`, `paragrafo`, `data_ricordo`, `tempo_ricordo`) VALUES ('$nome','$titolo','$paragrafo','$data','$ora')";
+
+    $risultato = mysqli_real_query($connection, $query);
+
+    if(!$risultato){
+        echo "Errore: query fallita.";
+        exit();
+    }
+
+    echo "<br><br>Successo.";
+
+
+?>
