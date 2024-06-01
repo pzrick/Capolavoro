432bd71e76b27dc0482f4a43d486dfc2a0ed4d6b
root <pizza.rick05@gmail.com>
2024-06-01 09:58:50 +0200

    aggiunta copia db

---
+++ ricordi.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 01, 2024 at 09:55 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ricordi2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ricordi`
+--
+
+CREATE TABLE `ricordi` (
+  `id_ricordo` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `titolo` varchar(40) NOT NULL,
+  `paragrafo` varchar(500) NOT NULL,
+  `data_ricordo` date NOT NULL,
+  `tempo_ricordo` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ricordi`
+--
+ALTER TABLE `ricordi`
+  ADD PRIMARY KEY (`id_ricordo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ricordi`
+--
+ALTER TABLE `ricordi`
+  MODIFY `id_ricordo` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
